796eec5a713a38ea2c47d21afbd4f288f993f2ef
EduardoA3677 <ealvarado261500@gmail.com>
2025-05-06 00:57:45 -0600

    Update RecordingsScreenContent.kt

--- feature/recordings/list/src/main/kotlin/com/f0x1d/logfox/feature/recordings/list/presentation/ui/compose/RecordingsScreenContent.kt
+++ feature/recordings/list/src/main/kotlin/com/f0x1d/logfox/feature/recordings/list/presentation/ui/compose/RecordingsScreenContent.kt
@@ -220,7 +220,7 @@ private fun RecordingItem(
             verticalArrangement = Arrangement.spacedBy(6.dp, Alignment.CenterVertically),
         ) {
             Text(
-                text = logRecording.title? = null,
+                text = logRecording.title,
                 color = MaterialTheme.colorScheme.onSurface,
                 fontSize = 20.sp,
                 fontWeight = FontWeight.Bold,
